fa31fe8bc72940a01708910fa249c7907efb9730
Ibrahim Chand <31962750+ibchand@users.noreply.github.com>
2022-03-21 12:20:01 -0400

    Revert "Implemented Chat"
    This reverts commit e6852485e814beb37a2a3ce46536ac25f2960044.

--- medical_records_platform/medical_records_platform/settings.py
+++ medical_records_platform/medical_records_platform/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapi.apps.MyapiConfig',
     'rest_framework',
-    'chat',
-    'channels',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# mysite/settings.py
-# Channels
-ASGI_APPLICATION = 'medical_records_platform.asgi.application'
