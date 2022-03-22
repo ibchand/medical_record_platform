64fd7bcc4b814a334b3fe9fdc473fe6f09f06eab
Ibrahim Chand <31962750+ibchand@users.noreply.github.com>
2022-03-22 11:35:41 -0400

    Added queue components

--- medical_records_platform/myapi/views.py
+++ medical_records_platform/myapi/views.py
@@ -6,6 +6,9 @@ from .serializers import RoleSerializer
 from .models import User
 from .models import Role
 
+from django.http import JsonResponse
+from time import sleep
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('user_id')
     serializer_class = UserSerializer
@@ -13,3 +16,10 @@ class UserViewSet(viewsets.ModelViewSet):
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all().order_by('user')
     serializer_class = RoleSerializer
+
+def index(request):
+    json_payload = {
+        "message": "Hello world!"
+    }
+    sleep(10)
+    return JsonResponse(json_payload)
